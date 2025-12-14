dc4ffc394d1647f1e5228f442cd9a99152a15356
Chase Johnson <cjohnson19@pm.me>
2025-12-14 11:59:54 -0600

    day 10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,7 +1,8 @@
 use core::fmt;
-use std::{fmt::format, str::FromStr};
+use std::{iter::Sum, str::FromStr};
 
 use itertools::Itertools;
+use z3::{Optimize, ast::Int};
 
 advent_of_code::solution!(10);
 
@@ -9,17 +10,8 @@ advent_of_code::solution!(10);
 struct Machine {
     lights: u32,
     switches: Vec<u32>,
-    joltage: (),
-}
-
-fn parse_switch(s: &str) -> u32 {
-    s.strip_prefix("(")
-        .unwrap()
-        .strip_suffix(")")
-        .unwrap()
-        .split(",")
-        .map(|idx| idx.parse::<usize>().unwrap())
-        .fold(0, |a, i| a | (1 << i))
+    switches_raw: Vec<Vec<usize>>,
+    joltage: Vec<u32>,
 }
 
 impl FromStr for Machine {
@@ -39,14 +31,39 @@ impl FromStr for Machine {
             .collect::<String>();
         let lights = u32::from_str_radix(&binary_str, 2).unwrap();
 
-        let switches: Vec<u32> = parts
+        let switches_raw: Vec<Vec<_>> = parts
+            .clone()
             .take_while(|s| s.starts_with("("))
-            .map(|switch_str| parse_switch(switch_str))
+            .map(|elem| {
+                elem.strip_prefix("(")
+                    .unwrap()
+                    .strip_suffix(")")
+                    .unwrap()
+                    .split(",")
+                    .map(|e| e.parse::<usize>().unwrap())
+                    .collect()
+            })
+            .collect();
+        let switches = switches_raw
+            .iter()
+            .map(|raw_val| raw_val.iter().fold(0, |a, i| a | (1 << i)))
+            .collect();
+
+        let joltage: Vec<u32> = parts
+            .last()
+            .unwrap()
+            .strip_prefix("{")
+            .unwrap()
+            .strip_suffix("}")
+            .unwrap()
+            .split(",")
+            .map(|i| i.parse::<u32>().unwrap())
             .collect();
         Ok(Self {
             lights,
             switches,
-            joltage: (),
+            switches_raw,
+            joltage,
         })
     }
 }
@@ -105,7 +122,36 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let machines: Machines = input.parse().unwrap();
+    let mut ans = 0u64;
+    for machine in machines.machines {
+        let o = Optimize::new();
+        let switch_flip_counts: Vec<_> = (0..machine.switches.len())
+            .map(|i| Int::new_const(format!("flip_{i}")))
+            .collect();
+        for switch_flip in &switch_flip_counts {
+            o.assert(&switch_flip.ge(0));
+        }
+        for idx in 0..machine.joltage.len() {
+            let r = z3::ast::Int::sum(
+                machine
+                    .switches_raw
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, s)| s.contains(&idx))
+                    .map(|(i, _)| switch_flip_counts[i].clone()),
+            )
+            .eq(machine.joltage[idx]);
+            o.assert(&r);
+        }
+        o.minimize(&z3::ast::Int::sum(switch_flip_counts.iter()));
+        o.check(&[]);
+        let model = o.get_model().unwrap();
+        let a = model.eval(&z3::ast::Int::sum(switch_flip_counts.iter()), true);
+        ans += a.unwrap().as_u64().unwrap();
+    }
+
+    Some(ans)
 }
 
 #[cfg(test)]
@@ -121,13 +167,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_parse_switch() {
-        let n = parse_switch("(1,2,4)");
-        assert_eq!(n, 0b10110)
+        assert_eq!(result, Some(33));
     }
 
     #[test]
